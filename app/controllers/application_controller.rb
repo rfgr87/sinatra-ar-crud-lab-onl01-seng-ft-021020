09cc4cd1e6c412342b6d5b6205b0dc38756d8230
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-25 23:52:49 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -9,5 +10,42 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+  end
+  
+  get '/articles/new' do 
+    erb :new
+  end
+  
+  post '/articles' do
+    @article = Article.create(params)
+    redirect to "/articles/#{ @article.id }"
+  end
+  
+  get '/articles' do
+    @articles = Article.all 
+    erb :index
+  end
+  
+  get '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    erb :show
+  end
+  
+  patch '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    @article.title = params[:title]
+    @article.content = params[:content]
+    @article.save
+    redirect to "/articles/#{ @article.id }"
+  end
+  
+  get '/articles/:id/edit' do
+    @article = Article.find_by(id: params[:id])
+    erb :edit
+  end
+  
+  delete '/articles/:id' do
+    Article.delete(params[:id])
+    erb :index
   end
 end
